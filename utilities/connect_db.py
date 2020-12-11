8a080ca340f54346e802256b1b067e6cd59ce8db
atique24@gmail.com <Powai@Mumbai47>
2020-12-11 11:32:18 +0000

    Added utlities method to read data from db

---
+++ utilities/connect_db.py
@@ -0,0 +1,30 @@
+import cx_Oracle
+import os
+
+
+def db(username,password,ipaddress,serviceid_or_serviceName):
+    os.environ['PATH'] = 'SOME PATH FROM ENVIRONMENT'
+    con = cx_Oracle.connect(username,password,ipaddress,serviceid_or_serviceName)
+    cur = con.cursor()
+
+    insert_new_record = "insert into TableName values (102,'someStringValue')"                # assuming the table has two coloumns
+
+    update_existing_record = "update TableName set columnName1 = 'SomeValue' where coloumnName2 = 'something'"  # update a particular column
+
+    delete_record = "delete TableName where columnValue= 'Value'"   #delete row
+
+    select_query = "select * from employees"
+
+    select_one_record = "select customerId from customers where city = 'Walla' order by customerID desc"
+
+
+    cur.execute(select_query)
+
+    for columns in cur:
+        print(columns[0], " ", columns[1], " ", columns[2])   # this will extract only 3 coloumns
+
+
+    print('connected')
+
+    con.close()
+    con.commit() # if any update,delete query is run
