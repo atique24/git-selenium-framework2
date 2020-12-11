bccc06de835830d70e7c592e9367b547624a935f
atique24@gmail.com <Powai@Mumbai47>
2020-12-11 20:20:59 +0000

    Updated the requirements.txt

--- utilities/connect_db.py
+++ utilities/connect_db.py
@@ -8,22 +8,13 @@ def db(username,password,ipaddress,serviceid_or_serviceName):
     cur = con.cursor()
 
     insert_new_record = "insert into TableName values (102,'someStringValue')"                # assuming the table has two coloumns
-
     update_existing_record = "update TableName set columnName1 = 'SomeValue' where coloumnName2 = 'something'"  # update a particular column
-
     delete_record = "delete TableName where columnValue= 'Value'"   #delete row
-
     select_query = "select * from employees"
-
     select_one_record = "select customerId from customers where city = 'Walla' order by customerID desc"
-
-
     cur.execute(select_query)
-
     for columns in cur:
         print(columns[0], " ", columns[1], " ", columns[2])   # this will extract only 3 coloumns
-
-
     print('connected')
 
     con.close()
