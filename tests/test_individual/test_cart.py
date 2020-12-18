8fca47f42185435d29acbee3843fbfc05d7dadc4
atique24@gmail.com <Powai@Mumbai47>
2020-12-18 19:46:10 +0000

    added BaseTest

--- tests/test_individual/test_cart.py
+++ tests/test_individual/test_cart.py
@@ -2,9 +2,10 @@ from pages.mobile_page.mobile_page import MobilePage
 from utilities.mark_test_status import MarkTestStatus
 import unittest
 import pytest
+from tests.base_test import BaseTest
 
-@pytest.mark.usefixtures("oneTimeSetup")
-class TestCart(unittest.TestCase):
+#@pytest.mark.usefixtures("oneTimeSetup")
+class TestCart(BaseTest,unittest.TestCase):
 
     @pytest.fixture(autouse=True)
     def classObject(self,oneTimeSetup):
--- tests/test_individual/test_compare.py
+++ tests/test_individual/test_compare.py
@@ -2,9 +2,11 @@ from pages.mobile_page.mobile_page import MobilePage
 from utilities.mark_test_status import MarkTestStatus
 import unittest
 import pytest
+from tests.base_test import BaseTest
 
-@pytest.mark.usefixtures("oneTimeSetup")
-class TestCompare(unittest.TestCase):
+#@pytest.mark.usefixtures("oneTimeSetup")
+
+class TestCompare(BaseTest,unittest.TestCase):
 
     @pytest.fixture(autouse=True)
     def classObject(self,oneTimeSetup):
