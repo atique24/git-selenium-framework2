a0433ce90c35c81483e2f09cc4ec2d22578cb5b1
atique24 <atique24@gmail.com>
2021-08-15 18:09:49 +0100

    Added class method to enable/disable screenshot based upon CLI input

--- base/WebDriverFactory.py
+++ base/WebDriverFactory.py
@@ -7,6 +7,8 @@ from datafiles import config
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import IEDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from msedge.selenium_tools import EdgeOptions
 
 
 class WebDriverFactory():
@@ -17,22 +19,33 @@ class WebDriverFactory():
 
     def get_browser_instance(self):
         if self.browser.lower() == "firefox":
-            driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+            options = FirefoxOptions()
+            options.add_argument("--headless")
+            #options.add_argument("--disable-gpu")
+            profile = webdriver.FirefoxProfile()
+            profile.accept_untrusted_certs = True
+            driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), firefox_profile=profile, options=options)
 
         elif self.browser.lower() == "chrome":
             chrome_options = Options()
+            chrome_options.add_argument('ignore-certificate-errors')
+            #chrome_options.add_argument("--disable-gpu")
             chrome_options.add_argument('headless')
             chrome_options.add_argument('--start-maximized')
             chrome_options.add_experimental_option('prefs', {'geolocation': True})
             chrome_options.add_experimental_option('useAutomationExtension', False)
             chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
             #driver = webdriver.Chrome(options=chrome_options, executable_path='drivers//chromedriver.exe')
-            driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
+            driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
-        elif self.browser.lower() == "Ie":
+        elif self.browser.lower() == "ie":
             driver = webdriver.Ie(IEDriverManager().install())
 
-        elif self.browser.lower() == "Edge":
+        elif self.browser.lower() == "edge":
+            options = EdgeOptions()
+            options.use_chromium = True
+            options.add_argument("headless")
+            options.add_argument("disable-gpu")
             driver = webdriver.Edge(EdgeChromiumDriverManager().install())
 
         else:
