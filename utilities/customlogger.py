d6e40a6dbf88565eede5f44926dc2dbe9fabf43f
atique24 <atique24@gmail.com>
2021-08-15 22:56:18 +0100

    Code Reformat

--- utilities/customlogger.py
+++ utilities/customlogger.py
@@ -1,7 +1,8 @@
 import logging
 import inspect
 
-def custom_logger(loglevel = logging.INFO):
+
+def custom_logger(loglevel=logging.INFO):
     loggername = inspect.stack()[1][3]
 
     logger = logging.getLogger(loggername)
@@ -12,7 +13,6 @@ def custom_logger(loglevel = logging.INFO):
 
     formatter = logging.Formatter('%(asctime)s: %(name)s: %(levelname)s: %(message)s', datefmt='%d:%m:%y %H:%M:%S')
     filehander.setFormatter(formatter)
-
 
     logger.addHandler(filehander)
 
