8c6d11aa8e64c9f1f8506ac6063093c3a5545746
atique24 <atique24@gmail.com>
2021-08-22 13:41:58 +0100

    Old Code

--- base/base_page.py
+++ base/base_page.py
@@ -1,30 +1,35 @@
-from base.SeleniumBase import SeleniumBase
-from utilities.util import Utilities
-from assertpy import assert_that
-
-
-class BasePage(SeleniumBase):
-    def __init__(self, driver):
-        super(BasePage, self).__init__(driver)
-        self.driver = driver
-        self.util = Utilities()
-
-    def verify_page_title(self, titleToVerify):
-        actualTitle = self.getTitle()
-        return self.util.verify_text_contains(actualText=actualTitle, expectedText=titleToVerify)
-
-    def verify_table_header(self, locator, locatorType, tableToVerify):
-        actualTableHeader = self.getTextElementList(locator, locatorType)
-        return self.util.listcompare(actualList=actualTableHeader, expectedList=tableToVerify)
-
-    def verify_value(self, locator, locatorType, attributeType, expectedValue):
-        result = self.getAttribute(locator, locatorType, attributeType)
-        return self.util.verify_value(actualValue=result, expectedValue=expectedValue)
-
-    def verify_text_substring(self, locator, locatorType, attributeType, expectedValue):
-        result = self.getAttribute(locator, locatorType, attributeType)
-        return self.util.verify_text_contains(actualText=result, expectedText=expectedValue)
-
-    def verify_css_attribute(self, locator, locatorType, attributeType, expectedValue):
-        result = self.get_value_of_css_property(locator, locatorType, attributeType)
-        return self.util.verify_text(actualText=result, expectedText=expectedValue)
+# from base.SeleniumBase import SeleniumBase
+# from utilities.util import Utilities
+# from assertpy import assert_that
+# from selenium.webdriver.support.wait import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as ec
+#
+#
+# class BasePage(SeleniumBase):
+#     def __init__(self, driver):
+#         super(BasePage, self).__init__(driver)
+#         self.driver = driver
+#         self.util = Utilities()
+#
+#     def verify_page_title(self, titleToVerify):
+#             actualTitle = self.getTitle()
+#             return self.util.verify_text_contains(actualText=actualTitle, expectedText=titleToVerify)
+#
+#         def verify_table_header(self, locator, locatorType, tableToVerify):
+#             actualTableHeader = self.getTextElementList(locator, locatorType)
+#             return self.util.listcompare(actualList=actualTableHeader, expectedList=tableToVerify)
+#
+#         def verify_value(self, locator, locatorType, attributeType, expectedValue):
+#             result = self.getAttribute(locator, locatorType, attributeType)
+#             return self.util.verify_value(actualValue=result, expectedValue=expectedValue)
+#
+#         def verify_text_substring(self, locator, locatorType, attributeType, expectedValue):
+#             result = self.getAttribute(locator, locatorType, attributeType)
+#             return self.util.verify_text_contains(actualText=result, expectedText=expectedValue)
+#
+#         def verify_css_attribute(self, locator, locatorType, attributeType, expectedValue):
+#             result = self.get_value_of_css_property(locator, locatorType, attributeType)
+#             return self.util.verify_text(actualText=result, expectedText=expectedValue)
+#
+#
+#
