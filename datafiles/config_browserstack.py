cb1904aaa1b71ebfbd07b771cec025e6a47275ec
atique24 <atique24@gmail.com>
2021-11-18 21:02:06 +0000

    Run test on browserstack

--- datafiles/config_browserstack.py
+++ datafiles/config_browserstack.py
@@ -1,10 +1,54 @@
-bb_url = "https://atiqueshaikh1:qjqY8RpG7xCmLJp846TU@hub-cloud.browserstack.com/wd/hub"
-browser_config = {
-      'os_version': '10',
-      'os': 'Windows',
-      'browser': 'chrome',
-      'browser_version': 'latest',
-      'name': 'Parallel Test1', # test name
-      'build': 'browserstack-build-1' # Your tests will be organized within this build
-      }
+bb_url = "Your Browserstack URL"
+key = 'Your Key'
+localIdentifier = 'local'
+# browser_config = {
+#       'os_version': '10',
+#       'os': 'Windows',
+#       'browser': 'firefox',
+#       'acceptSslCerts': 'true',
+#       'browser_version': '46.0',
+#       'name': 'Demo Test', # test name
+#       'build': 'browserstack-build-1',
+#       'browserstack.local': 'true',
+#       'browserstack.localIdentifier': 'local',
+#       'resolution' : '2048x1536'
+#       }
 
+browser_config = {
+
+      'os_version': 'Big Sur',
+      'os': 'OS X',
+      'browser': 'safari',
+      'browser_version': 'latest',
+      'name': 'Parallel Test3',
+      'build': 'browserstack-build-1',
+      'browserstack.local': 'true',
+      'browserstack.localIdentifier': 'local',
+      #'resolution': '2048x1536',
+      'acceptSslCerts': 'true',
+}
+
+# browser_config = {
+#       'realMobile': 'true',
+#       'browserName': 'android',
+#       'device': 'Samsung Galaxy S21 Ultra',
+#       'os_version': '11.0',
+#       'name': 'Parallel Test1',  # test name
+#       'build': 'browserstack-build-1',
+#       'browserstack.local': 'true',
+#       'browserstack.localIdentifier': 'local',
+#       'acceptSslCerts': 'true',
+#        }
+
+# browser_config = {
+#       'realMobile': 'true',
+#       'browserName': 'ios',
+#       'device': 'iPhone 12 Pro Max',
+#       'os_version': '14',
+#       'name': 'Parallel Test3',
+#       'build': 'browserstack-build-1',
+#       'browserstack.local': 'true',
+#       'browserstack.localIdentifier': 'local',
+#       'acceptSslCerts': 'true',
+#
+# }
