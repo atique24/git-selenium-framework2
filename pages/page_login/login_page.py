71f1233965d30160dcd2cebbc660ce174db63203
atique24 <atique24@gmail.com>
2022-09-06 23:01:29 +0100

    Updated the Page Class

--- pages/page_login/login_page.py
+++ pages/page_login/login_page.py
@@ -1,34 +1,38 @@
-from pages.locators import LoginPageLocators
+from selenium.webdriver.common.by import By
 from base.SeleniumBase import SeleniumBase
 import allure
 from assertpy import assert_that
 
 
-class Login(SeleniumBase):
+class LoginPage(SeleniumBase):
+    username = (By.NAME, "userName")
+    password = (By.NAME, "password")
+    submit = (By.NAME, "submit")
+    success_message = (By.XPATH, "//h3[text()='Login Successfully']")
+    sign_off = (By.LINK_TEXT, "SIGN-OFF")
 
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
-        self.lp = LoginPageLocators()
 
     @allure.step("Enter the username")
-    def enter_username(self,username):
-        self.elementSend(self.lp.username, username)
+    def enter_username(self, username):
+        self.elementSend(self.username, username)
 
     @allure.step("Enter the password")
-    def enter_password(self,password):
-        self.elementSend(self.lp.password, password)
+    def enter_password(self, password):
+        self.elementSend(self.password, password)
 
     @allure.step("Click on Submit button")
     def click_submit_button(self):
-        self.elementClick(self.lp.submit)
+        self.elementClick(self.submit)
 
     @allure.step("Check if the success message is displayed")
     def check_login_successfull(self):
-        #assert_that(self.isElementDisplayed(self.lp.success_message)).is_true()
-        return self.isElementDisplayed(self.lp.success_message)
+        # assert_that(self.isElementDisplayed(self.lp.success_message)).is_true()
+        return self.isElementDisplayed(self.success_message)
 
     @allure.step("Check if the sign off link is displayed")
     def check_sign_off_link_is_displayed(self):
-        return self.isElementDisplayed(self.lp.sign_off)
-        #assert_that(self.isElementDisplayed(self.lp.sign_off)).is_true()
+        return self.isElementDisplayed(self.sign_off)
+        # assert_that(self.isElementDisplayed(self.lp.sign_off)).is_true()
--- pages/page_register/register_page.py
+++ pages/page_register/register_page.py
@@ -1,36 +1,49 @@
-from pages.locators import RegisterLocators
+from selenium.webdriver.common.by import By
 from base.SeleniumBase import SeleniumBase
 import allure
 
 
 class Registration(SeleniumBase):
+    accept_privacy = (By.CSS_SELECTOR, "#save")
+    register = (By.LINK_TEXT, "REGISTER")
+    firstname = (By.NAME, "firstName")
+    lastname = (By.NAME, "lastName")
+    phone = (By.NAME, "phone")
+    email = (By.NAME, "userName")
+    submit = (By.NAME, "submit")
+    success_message = (By.XPATH, "//font[contains(text(),'Thank you')]")
 
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
-        self.locators = RegisterLocators
 
+    @allure.step("Accepting the Privacy Policy")
+    def accept_privacy_message(self):
+        self.wait_and_switch_Iframe(index=6)
+        self.elementClick(self.accept_privacy, force=True)
+        self.exitIframe()
 
     @allure.step("Clicking on register from homepage")
     def click_register_button(self):
-        self.elementClick(self.locators.register)
+        self.wait_for_element_to_be_visible(self.register)
+        self.moveToElementAndClick(self.register)
 
     @allure.step("Enter the firstname")
-    def enter_firstname(self,firstname):
-        self.elementSend(self.locators.firstname, firstname)
+    def enter_firstname(self, firstname):
+        self.elementSend(self.firstname, firstname)
 
     @allure.step("Enter the lastname")
-    def enter_lastname(self,lastname):
-        self.elementSend(self.locators.lastname, lastname)
+    def enter_lastname(self, lastname):
+        self.elementSend(self.lastname, lastname)
 
     @allure.step("Enter the email")
     def enter_email(self, email):
-        self.elementSend(self.locators.email, email)
+        self.elementSend(self.email, email)
 
     @allure.step("Click on Submit button")
     def click_submit_button(self):
-        self.elementClick(self.locators.submit)
+        self.elementClick(self.submit)
 
     @allure.step("Check if the success message is displayed")
     def check_registration_message(self):
-        return self.isElementDisplayed(self.locators.success_message)
+        return self.isElementDisplayed(self.success_message)
